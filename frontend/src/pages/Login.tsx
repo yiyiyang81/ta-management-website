260e6de355a4681f0188ec8dd55c3c315dafe1b5
Marlene Liang <marlene.liang@hotmail.com>
2022-12-11 13:22:25 -0500

    Implemented frontend for landing page + registration + reusable form components

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import logo from "../assets/images/mcgill_logo.jpg";
+import logo from "../assets/images/mcgill-logo.png";
+import socsLogo from "../assets/images/socs-logo.png";
 import { UserContext } from "../App";
 import "../App.css";
 import "../style/login.css";
+import Button from "../common/Button";
 
 const Login: React.FC = () => {
   // Load global state
@@ -30,19 +32,16 @@ const Login: React.FC = () => {
     try {
       // Make login API call
       // CAUTION: Do not hardcode the URLs, instead use routers
-      const res = await fetch(
-        "http://127.0.0.1:3000/api/users/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-        }
-      );
+      const res = await fetch("http://127.0.0.1:3000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
 
       // If login was successful, set user and redirect to home page
       if (res.status === 200) {
@@ -61,46 +60,64 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="login">
-      <div className="welcome">
-        <form onSubmit={submitHandler}>
-          <div className="form-inner">
-            <img className="logo" src={logo} alt="mcgill-logo" />
-
-            <p className="top">Sign in with your email and password.</p>
-            {error !== "" ? <div className="error"> * {error} </div> : ""}
-
-            <div className="form-group">
-              <input
-                type="text"
-                name="email"
-                placeholder="email"
-                id="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                id="password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-
-            <div className="sign-in-button">
-              <input type="submit" value="Sign in" />
-            </div>
-
-            <p className="bottom">
-              <Link className="links" to="/login">
-                Register
-              </Link>
-            </p>
+    <div className="landing-container">
+      <div className="login-container">
+        <div className="login">
+          <div>
+            <img className="logo" src={socsLogo} alt="socs-logo" />
           </div>
-        </form>
+          <div>
+            <img className="logo" src={logo} alt="mcgill-logo" />
+          </div>
+          <form onSubmit={submitHandler}>
+            <div className="form-inner">
+              <p className="top">Sign in with your email and password.</p>
+              
+              {error !== "" ? <div className="error"> * {error} </div> : ""}
+
+              <div className="login-form-group mb-2">
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div className="login-form-group mb-2">
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <Button type="submit" value="Log In"></Button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="registration-container">
+        <div className="registration">
+          <div className="mb-5">
+            <h1 className="mb-4">Welcome to myTAs! </h1>
+            The teaching assistant management website designed to enhance your
+            learning experience. <br />
+            As a student and a teacher, you’ll be able to learn more about your
+            TAs. <br />
+            As a teaching assistant, you’ll be able to get instant feedback and
+            connect with your students
+          </div>
+
+          <Link to="/register">
+            <div className="register-button">
+            <Button type="primary" value="Register"></Button>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
