323f162786e4b56af3d28862789faf7f12b9a236
yiyiyang81 <yiyi.yang3@mail.mcgill.ca>
2022-12-12 20:19:15 -0500

    add URLs to link

--- frontend/src/components/RoleTabs.tsx
+++ frontend/src/components/RoleTabs.tsx
@@ -1,19 +1,44 @@
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import { Container, Nav, Navbar, NavItem, NavDropdown, Tab, Tabs } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 import "../style/primaryTopbar.css";
 
 
-function RoleTabs(props:any) {
-  return (
-    <Tabs
-      defaultActiveKey="rate"
-      className="primary mb-3"
-    >
-      <Tab eventKey="manage" title="TA Management"/>
-      <Tab eventKey="admin" title="TA Administration" disabled/>
-      <Tab eventKey="rate" title="Rate a TA" />
-    </Tabs>
-  );
+
+function RoleTabs(props: any) {
+    const navigate = useNavigate();
+
+    const handleSelect = (eventKey) => {
+        let route;
+        // TODO: change keys
+        switch (eventKey) {
+            case 1:
+                route = 'link1';
+                break;
+            case 2:
+                route = 'link2';
+                break;
+            case 3:
+                route = 'link3';
+                break;
+            default:
+                route = '/';
+        }
+        navigate(route)
+    }
+
+    return (
+        <Tabs
+            defaultActiveKey={3}
+            className="primary mb-3"
+            onSelect={handleSelect}
+        >
+            <Tab eventKey={0} title="Dashboard" />
+            <Tab eventKey={1} title="TA Management" />
+            <Tab eventKey={2} title="TA Administration" disabled />
+            <Tab eventKey={3} title="Rate a TA" />
+        </Tabs>
+    );
 }
 
 export default RoleTabs;
