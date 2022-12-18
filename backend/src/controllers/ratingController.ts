d65301693d239b7009f2c46a65db14c701c00103
Marlene Liang <marlene.liang@hotmail.com>
2022-12-17 23:32:29 -0500

    Implemented rating model, controller, and routes

---
+++ backend/src/controllers/ratingController.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import { CourseHelper } from "../helpers/courseHelper";
+import Rating from "../models/Rating";
+import { TAHelper } from "../helpers/taHelper";
+import { RatingHelper } from "../helpers/ratingHelper";
+
+// @Desc Get all ratings
+// @Route /api/ratings
+// @Method GET
+export const getAllRatings = asyncHandler(async (req: Request, res: Response) => {
+	const ratings = await Rating.find({});
+	res.status(200).json({
+		ratings
+	});
+});
+
+// @Desc Get a list of Ratings by the email
+// @Route /api/ratings/:email
+// @Method GET
+export const getRatingsByEmail = asyncHandler(async (req: Request, res: Response) => {
+	const email = req.params.email
+	const ratings = await Rating.find({ email: email })
+	res.status(200).json({
+		ratings
+	});
+
+});
+
+// @Desc Get a list of Ratings by the email and course number
+// @Route /api/ratings/:email/:courseNumber
+// @Method GET
+export const getRatingsByEmailAndCourseNumber = asyncHandler(async (req: Request, res: Response) => {
+	const { email, courseNumber } = req.params
+	const ratings = await Rating.find({ email: email, course_number: courseNumber })
+	res.status(200).json({
+		ratings
+	});
+});
+
+// @Desc Get a list of comments about the TA associated with the course 
+// by the email of a TA and a course number 
+// @Route /api/ratings/comments/:email/:courseNumber
+// @Method GET
+export const getRatingsCommentsByEmailAndCourseNumber = asyncHandler(async (req: Request, res: Response) => {
+	const { email, courseNumber } = req.params
+	const ratings = await Rating.find({ email: email, course_number: courseNumber })
+	let comments = []
+	for (let i = 0; i < ratings.length; i++) {
+		comments.push(ratings[i].comment)
+	}
+	res.status(200).json({
+		email: email,
+		course_number: courseNumber,
+		comments: comments
+	});
+});
+
+// @Desc Add a rating
+// @Route /api/ratings/add
+// @Method POST
+export const addRating = asyncHandler(async (req: Request, res: Response) => {
+	const { course_number, email, rating_score, comment } = req.body
+	const courseId = await CourseHelper.getCourseIdByCourseNumber(course_number)
+	const teachingAssistantId = await TAHelper.getTAIdByEmail(email)
+	const rating = await RatingHelper.createRatingsDb(course_number, courseId, email, teachingAssistantId, rating_score, comment)
+	res.status(200).json({
+		rating
+	});
+});
---
+++ backend/src/models/Rating.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from 'mongoose';
+import { ICourse } from './Course';
+import { ITA } from './TA';
+
+export interface IRating extends mongoose.Document {
+	course_number: string,
+	course: ICourse,
+	teaching_assistant: ITA,
+	email: string,
+	rating_score: number,
+	comment: string, 
+	createdAt: Date,
+	updatedAt: Date,
+}
+
+const UserSchema = new mongoose.Schema({
+	course_number: {
+		type: String,
+		required: true,
+	},
+	course: {
+		type: Schema.Types.ObjectId,
+		required: true
+	},
+	teaching_assistant: {
+		type: Schema.Types.ObjectId,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	rating_score: {
+		type: Number,
+		required: true
+	},
+	comment: {
+		type: String,
+		required: true
+	},
+	createdAt: Date,
+	updatedAt: Date,
+}, {
+	timestamps: true
+})
+
+const Rating = mongoose.model<IRating>("Rating", UserSchema);
+export default Rating;
+
+
