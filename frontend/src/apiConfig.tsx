a89018e2a97ecab71f2a11ca7d80214a57687cd8
Yiyi Yang <57012409+yiyiyang81@users.noreply.github.com>
2023-07-13 18:42:11 -0400

    Update apiConfig.tsx

--- frontend/src/apiConfig.tsx
+++ frontend/src/apiConfig.tsx
@@ -1,6 +1,7 @@
 const isDevelopment = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
 const DEV_BACKEND_ROUTE = "http://localhost:5000"
-const PROD_BACKEND_ROUTE = "https://fall2022-comp307-group6.cs.mcgill.ca" // change with mimi server 
+//const PROD_BACKEND_ROUTE = "https://fall2022-comp307-group6.cs.mcgill.ca" // change with mimi server
+const PROD_BACKEND_ROUTE = "https://main.d3mg8e6fi80dx8.amplifyapp.com/"
 export const BACKEND_ROUTE = isDevelopment ? DEV_BACKEND_ROUTE : PROD_BACKEND_ROUTE
 
 export const callBackend = async (route : string,params : RequestInit = {}) => {
